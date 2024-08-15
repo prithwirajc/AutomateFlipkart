804948bbbeda8ed83f35adeaa16840d6fd3ad1e9
prithwirajchakraborty <prithwirajchakraborty97@gmail.com>
2024-08-15 16:43:05 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,17 +1,20 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.util.*;
 import java.util.logging.Level;
 
 
@@ -20,19 +23,20 @@ import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers;
+    WebDriverWait wait;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
+     * TODO: Write your tests here with testng @Test annotation.
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
-     
+
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,18 +50,134 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
+
+        wrappers = new Wrappers(driver);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         driver.manage().window().maximize();
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
     }
+
+    @Test
+    public void testCase01() {
+        try {
+
+            wrappers.openFlipkart();
+            wrappers.searchItems("Washing Machine");
+            WebElement populatiry = driver.findElement(By.xpath("//div[text()='Popularity']"));
+            populatiry.click();
+            List<WebElement> itemList = driver.findElements(By.cssSelector(".cPHDOP.col-12-12"));
+            int count = 0;
+
+            for (WebElement e : itemList) {
+                try {
+                    WebElement rating = e.findElement(By.className("XQDdHH"));
+                    float num = 4.0f;
+                    float f = Float.parseFloat(rating.getText());
+                    if (f <= num) {
+                        count++;
+                    }
+                } catch (Exception ex) {}
+            }
+            System.out.println(count);
+            assert count > 0;
+            Thread.sleep(1000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert false;
+        }
+
+
+    }
+
+    @Test
+    public void testCase02() {
+        wrappers.openFlipkart();
+        wrappers.searchItems("iPhone");
+        List<WebElement> itemList = driver.findElements(By.cssSelector(".cPHDOP.col-12-12"));
+        System.out.println(itemList.size());
+
+        int count = 0;
+
+        for (WebElement e : itemList) {
+            try {
+                WebElement discount = e.findElement(By.className("UkUFwK"));
+                String discountText = discount.getText();
+                discountText = discountText.replace("% off", "");
+                float disc = Float.parseFloat(discountText);
+                if (disc > 17.0f) {
+                    WebElement itemName = e.findElement(By.className("KzDlHZ"));
+                    System.out.println(itemName.getText() + " :-Discount% = " + discountText + "%");
+                    count++;
+                }
+
+
+            } catch (Exception ignored) {
+
+            }
+        }
+
+
+    }
+
+    @Test
+    public void testCase03() {
+
+        wrappers.openFlipkart();
+        wrappers.searchItems("Coffee Mug");
+        WebElement fourStarCheckBox = driver.findElement(By.xpath("(//div[@class='XqNaEv'])[1]"));
+        fourStarCheckBox.click();
+
+        wrappers.sleep(5000);
+
+        List<WebElement> itemList = driver.findElements(By.className("slAVV4"));
+        System.out.println(itemList.size());
+        TreeMap<Integer, WebElement> reviewMap = new TreeMap<>(Collections.reverseOrder());
+
+        for (WebElement e : itemList){
+            try {
+                String review = e.findElement(By.className("Wphh3N")).getText();
+                int reviewInt = wrappers.getReview(review);
+
+                if (!reviewMap.containsKey(reviewInt)){
+                    reviewMap.put(reviewInt,e);
+                }
+
+            } catch (Exception ignored) {
+                ignored.printStackTrace();
+            }
+        }
+
+
+        int count = 0;
+        for (Map.Entry<Integer, WebElement> entry : reviewMap.entrySet()) {
+            if (count >= 5) break;
+            WebElement item = entry.getValue();
+            String title = item.findElement(By.className("wjcEIp")).getText();
+            String imageUrl = item.findElement(By.className("DByuf4")).getAttribute("src");
+            System.out.println("Title: " + title);
+            System.out.println("Image URL: " + imageUrl);
+            count++;
+        }
+
+
+
+
+
+
+    }
 }
+
+
+
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -19,4 +20,48 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    ChromeDriver driver;
+
+
+    public Wrappers(ChromeDriver driver){
+        this.driver = driver;
+    }
+
+
+    public void searchItems(String searchKey){
+        WebElement searchInp=driver.findElement(By.className("Pke_EE"));
+        searchInp.sendKeys(searchKey);
+        searchInp.sendKeys(Keys.ENTER);
+    }
+
+    public void openFlipkart(){
+        driver.get("https://www.flipkart.com/");
+    }
+    public void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
+    public int getReview(String review) throws Exception {
+        StringBuilder rev = new StringBuilder();
+        for (int i = 0; i < review.length(); i++){
+            if (Character.isDigit(review.charAt(i)))
+                rev.append(review.charAt(i));
+        }
+
+        if (rev.length() > 0){
+            return Integer.parseInt(rev.toString());
+        }else
+            throw new Exception("Invalid Review");
+    }
+
+
+
+
+
 }
